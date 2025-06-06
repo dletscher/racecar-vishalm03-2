28833916488856f078f17ab8597a9277a0b8943f
Vishal Muniraj <vishal.muniraj@slu.edu>
2025-06-06 15:14:13 -0500

    changes made in MyAgent.py

--- MyAgent.py
+++ MyAgent.py
@@ -5,23 +5,27 @@ class Agent:
         lidar = observations['lidar']
         velocity = observations['velocity']
 
-        left, mid_left  , center, mid_right, right = lidar
+        left, mid_left, center, mid_right, right = lidar
         side_balance = left - right
-        if velocity < 0.05:
+        if velocity < 0.09:
             return ('straight','accelerate')
-        if left < 0.2:
-            return('right','brake')
-        if right < 0.2:
-            return('left','brake')
-        if side_balance > 0.25:
+        if center == 2.0:
+            return('straight', 'coast')
+        if left < 0.15:
+            return('right','coast')
+        if right < 0.15:
+            return('left','coast')
+        if center > 3.5 and center < 5.0:
+            return('straight', 'accelerate')
+        if side_balance > 0.3:
             return('left','brake')
         elif side_balance > 0.1:
-            return('left','coast')
-        elif side_balance < -0.25:
+            return('left','accelerate')
+        elif side_balance < -0.3:
             return('right','brake')
         elif side_balance < -0.1:
-            return('right','coast')
-        if velocity < 0.5:
+            return('right','accelerate')
+        if velocity < 0.9:
             return('straight','accelerate')
         else:
             return('straight','coast') 
